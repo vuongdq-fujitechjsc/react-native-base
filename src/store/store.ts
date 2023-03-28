3b26ac318de591633c76fa5bbb598f9461db25cb
Vuong Do Quang <vuong.do.quang@ofatechjsc.com>
2023-03-28 17:33:17 +0700

    Format code base redux

---
+++ src/store/store.ts
@@ -0,0 +1,31 @@
+import { Action, ThunkAction, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+import createSagaMiddleware from "@redux-saga/core";
+import logger from 'redux-logger';
+import { persistedReducer } from "src/reducers/rootReducer";
+import rootSaga from "src/sagas/rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+sagaMiddleware.run(rootSaga);
+
+export const appStore = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false
+        }).concat(
+            sagaMiddleware,
+            logger
+        )
+});
+
+export type AppDispatch = typeof appStore.dispatch;
+
+export type RootState = ReturnType<typeof appStore.getState>;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
